3d67437713fb3597cd5fa75607df1ee833c817e6
Yasser González Fernández <ygonzalezfernandez@gmail.com>
2012-02-21 08:46:23 -0500

    Tabulated the CvM statistic of the independence test (incomplete table).

--- src/measure/cvm.R
+++ src/measure/cvm.R
@@ -1,9 +1,8 @@
 library("copula")
 
-cat("{\n")
-for (n in seq(from = 100, to = 1000, by = 100)) {
+N <- c(seq(from = 10, to = 90, by = 10), seq(from = 100, to = 10000, by = 100))
+for (n in N) {
     sim <- indepTestSim(n, p = 2, N = 100, print.every = -1)
     content <- paste(sim$dist.global.statistic.independence, collapse = ", ")
     cat("    {", content, "},\n", sep = "")
 }
-cat("};\n")
