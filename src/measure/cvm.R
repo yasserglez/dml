1c5ad20758cc0118d4ecce25e5303487c4714d88
Yasser González Fernández <ygonzalezfernandez@gmail.com>
2012-02-22 11:07:08 -0500

    Added simulations of the CvM statistic of the independence test.

--- src/measure/cvm.R
+++ src/measure/cvm.R
@@ -4,5 +4,5 @@ N <- c(seq(from = 10, to = 90, by = 10), seq(from = 100, to = 10000, by = 100))
 for (n in N) {
     sim <- indepTestSim(n, p = 2, N = 100, print.every = -1)
     content <- paste(sim$dist.global.statistic.independence, collapse = ", ")
-    cat("    {", content, "},\n", sep = "")
+    cat("    /* n = ", n, " */ {", content, "},\n", sep = "")
 }
